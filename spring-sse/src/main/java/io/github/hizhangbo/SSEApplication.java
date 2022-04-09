69242b584176da84e722ce708a293edcb67ed5a3
bob <hizhangbo@outlook.com>
2022-04-10 00:36:44 +0800

    test

--- spring-sse/src/main/java/io/github/hizhangbo/SSEApplication.java
+++ spring-sse/src/main/java/io/github/hizhangbo/SSEApplication.java
@@ -6,6 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 /**
  * @author Bob
  * @date 2020-02-02 12:04
+ * Server-Sent Events 简称SSE
+ * 服务器向客户端进行单向多次推送，替换WebSocket
  */
 @SpringBootApplication
 public class SSEApplication {
